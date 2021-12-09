6a079f830a5d4dcf49dbde5cd238d8a0327e5bbb
Daniel Amarante <daniel.amarante@truecaller.com>
2021-12-09 21:43:23 +0100

    Add back aprt 1 solution

--- 2021/7/7.swift
+++ 2021/7/7.swift
@@ -6,19 +6,25 @@ let crabs = input
     .split(separator: ",")
     .compactMap { Int($0) }
 
-var minimumSum = Int.max
-for i in crabs.min()!...crabs.max()! {
-    let sum = crabs.reduce(0) { acc, crab in
-        acc + calculateFuel(steps: abs(crab - i))
-    }
-    if sum < minimumSum {
-        minimumSum = sum
-    }
-}
-
 func calculateFuel(steps: Int) -> Int {
     if steps == 0 { return steps }
     return steps + calculateFuel(steps: steps - 1)
 }
 
-print(minimumSum)
+func solve(crabs: [Int], calculateFuel: (Int) -> Int) -> Int {
+    var minimumSum = Int.max
+    for i in crabs.min()!...crabs.max()! {
+        let sum = crabs.reduce(0) { acc, crab in
+            acc + calculateFuel(abs(crab - i))
+        }
+        if sum < minimumSum {
+            minimumSum = sum
+        }
+    }
+    return minimumSum
+}
+
+// Part 1
+print(solve(crabs: crabs, calculateFuel: { $0 }))
+// Part 2
+print(solve(crabs: crabs, calculateFuel: calculateFuel(steps:)))

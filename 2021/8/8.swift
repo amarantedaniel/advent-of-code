2f583b774b97b4b5f271f6e491707b2d595fb88a
Daniel Amarante <daniel.amarante@truecaller.com>
2021-12-10 21:19:53 +0100

    Add solution for day 8

---
+++ 2021/8/8.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+extension Set {
+    mutating func pop(where block: (Element) -> Bool) -> Element? {
+        guard let element = first(where: block) else { return nil }
+        remove(element)
+        return element
+    }
+}
+
+let input = try! String(contentsOfFile: "input.txt", encoding: .utf8)
+    .split(separator: "\n")
+
+struct Signal {
+    let patterns: Set<Set<Character>>
+    let outputs: [Set<Character>]
+}
+
+var signals: [Signal] = []
+
+for line in input {
+    let splitted = line.components(separatedBy: " | ")
+    let patterns = splitted[0].components(separatedBy: " ").map { Set($0) }
+    let outputs = splitted[1].components(separatedBy: " ").map { Set($0) }
+    signals.append(Signal(patterns: Set(patterns), outputs: outputs))
+}
+
+var sum = 0
+
+for signal in signals {
+    var patterns = signal.patterns
+    let outputs = signal.outputs
+    var lookup: [Set<Character>: Int] = [:]
+
+    let one = patterns.pop(where: { $0.count == 2 })!
+    lookup[one] = 1
+    let four = patterns.pop(where: { $0.count == 4 })!
+    lookup[four] = 4
+    let seven = patterns.pop(where: { $0.count == 3 })!
+    lookup[seven] = 7
+    let eight = patterns.pop(where: { $0.count == 7 })!
+    lookup[eight] = 8
+    let nine = patterns.pop(where: { $0.isStrictSuperset(of: four) && $0.count == 6 })!
+    lookup[nine] = 9
+    let zero = patterns.pop(where: { $0 != nine && $0.count == 6 && $0.isStrictSuperset(of: one) })!
+    lookup[zero] = 0
+    let six = patterns.pop(where: { $0 != nine && $0 != zero && $0.count == 6 })!
+    lookup[six] = 6
+    let two = patterns.pop(where: { !$0.isStrictSubset(of: nine) })!
+    lookup[two] = 2
+    let three = patterns.pop(where: { $0.isStrictSuperset(of: one) })!
+    lookup[three] = 3
+    let five = patterns.popFirst()!
+    lookup[five] = 5
+
+    sum += Int(outputs.compactMap { lookup[$0]?.description }.joined())!
+}
+
+print(sum)

25602ef87bc6bce0df99cdce5e9443b8a1763f1b
Daniel Amarante <daniel.amarante@truecaller.com>
2021-12-14 20:06:30 +0100

    Solve day 11

--- Y2021/Sources/Y2021D10/Solution.swift
+++ Y2021/Sources/Y2021D10/Solution.swift
@@ -1,9 +1,24 @@
 import Foundation
 
-let score: [Character: Int] = [")": 3, "]": 57, "}": 1197, ">": 25137]
-let matching: [Character: Character] = [")": "(", "]": "[", "}": "{", ">": "<"]
+enum SyntaxError {
+    case wrongCharacter(character: Character)
+    case missingCharacter(stack: [Character])
 
-func analyze(chunk: Substring) -> Int {
+    var isWrongCharacterError: Bool {
+        if case .wrongCharacter = self { return true }
+        return false
+    }
+
+    var isMissingCharacterError: Bool {
+        if case .missingCharacter = self { return true }
+        return false
+    }
+}
+
+func analyze(chunk: Substring) -> SyntaxError? {
+    let openingEquivalent: [Character: Character] = [
+        ")": "(", "]": "[", "}": "{", ">": "<"
+    ]
     var stack: [Character] = []
 
     for character in chunk {
@@ -11,22 +26,45 @@ func analyze(chunk: Substring) -> Int {
         case "[", "{", "<", "(":
             stack.append(character)
         case "]", "}", ">", ")":
-            if stack.popLast() != matching[character] {
-                return score[character] ?? 0
+            if stack.popLast() != openingEquivalent[character] {
+                return .wrongCharacter(character: character)
             }
         default:
             break
         }
     }
-    return 0
+    if !stack.isEmpty {
+        return .missingCharacter(stack: stack)
+    }
+    return nil
 }
 
-func solve1(input: String) -> Int {
-    input.split(separator: "\n").reduce(0) { sum, chunk in
-        sum + analyze(chunk: chunk)
+func calculateScore(error: SyntaxError) -> Int {
+    switch error {
+    case .wrongCharacter(let character):
+        let scoreMap: [Character: Int] = [")": 3, "]": 57, "}": 1197, ">": 25137]
+        return scoreMap[character]!
+    case .missingCharacter(let stack):
+        let scoreMap: [Character: Int] = ["(": 1, "[": 2, "{": 3, "<": 4]
+        return stack.reversed().reduce(0) { score, character in
+            (score * 5) + scoreMap[character]!
+        }
     }
 }
 
+func solve1(input: String) -> Int {
+    return input.split(separator: "\n")
+        .compactMap { chunk in analyze(chunk: chunk) }
+        .filter(\.isWrongCharacterError)
+        .reduce(0) { sum, error in
+            sum + calculateScore(error: error)
+        }
+}
+
 func solve2(input: String) -> Int {
-    return 0
+    let results = input.split(separator: "\n")
+        .compactMap { chunk in analyze(chunk: chunk) }
+        .filter(\.isMissingCharacterError)
+        .map { error in calculateScore(error: error) }
+    return results.sorted()[results.count / 2]
 }

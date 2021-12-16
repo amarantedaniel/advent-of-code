8b865c0fe454952d96337bd5b797195de1891ace
Daniel Amarante <daniel.amarante@truecaller.com>
2021-12-16 18:09:46 +0100

    Add part 2 for day 12

--- Y2021/Sources/Y2021D12/Solution.swift
+++ Y2021/Sources/Y2021D12/Solution.swift
@@ -1,19 +1,30 @@
 import Foundation
 
-func countRoutes(from cave: Cave) -> Int {
+func countRoutes(from cave: Cave, canVisitTwice: Bool) -> Int {
     if case .end = cave.variant {
         return 1
     }
     var routes = 0
-    for neighboor in cave.neighboors where !neighboor.visited {
-        neighboor.visit()
-        routes += countRoutes(from: neighboor)
-        neighboor.visited = false
+    for neighboor in cave.neighboors {
+        if neighboor.visited {
+            if canVisitTwice, case .small = neighboor.variant {
+                routes += countRoutes(from: neighboor, canVisitTwice: false)
+            }
+        } else {
+            neighboor.visit()
+            routes += countRoutes(from: neighboor, canVisitTwice: canVisitTwice)
+            neighboor.visited = false
+        }
     }
     return routes
 }
 
 func solve1(input: String) -> Int {
     let start = Parser.parse(input: input)
-    return countRoutes(from: start)
+    return countRoutes(from: start, canVisitTwice: false)
+}
+
+func solve2(input: String) -> Int {
+    let start = Parser.parse(input: input)
+    return countRoutes(from: start, canVisitTwice: true)
 }
--- Y2021/Tests/Y2021D12Tests/SolutionTests.swift
+++ Y2021/Tests/Y2021D12Tests/SolutionTests.swift
@@ -26,4 +26,28 @@ class SolutionTests: XCTestCase {
         let input = try! String(contentsOfFile: path, encoding: .utf8)
         XCTAssertEqual(solve1(input: input), 5104)
     }
+
+    func test_solve2_withSampleInput() {
+        let path = Bundle.module.path(forResource: "sample", ofType: "txt")!
+        let input = try! String(contentsOfFile: path, encoding: .utf8)
+        XCTAssertEqual(solve2(input: input), 36)
+    }
+
+    func test_solve2_withMediumSampleInput() {
+        let path = Bundle.module.path(forResource: "sample-medium", ofType: "txt")!
+        let input = try! String(contentsOfFile: path, encoding: .utf8)
+        XCTAssertEqual(solve2(input: input), 103)
+    }
+
+    func test_solve2_withLargeSampleInput() {
+        let path = Bundle.module.path(forResource: "sample-large", ofType: "txt")!
+        let input = try! String(contentsOfFile: path, encoding: .utf8)
+        XCTAssertEqual(solve2(input: input), 3509)
+    }
+
+    func test_solve2_withLargeInput() {
+        let path = Bundle.module.path(forResource: "large", ofType: "txt")!
+        let input = try! String(contentsOfFile: path, encoding: .utf8)
+        XCTAssertEqual(solve2(input: input), 149220)
+    }
 }

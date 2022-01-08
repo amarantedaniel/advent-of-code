5f92146ddec0c5bfb5006a667410d96b51443699
Daniel Amarante <daniel.amarante@truecaller.com>
2022-01-08 16:55:36 +0100

    Solve day 22

--- Y2021/Sources/Day22/Solution.swift
+++ Y2021/Sources/Day22/Solution.swift
@@ -1,26 +1,40 @@
 import Foundation
 
-struct Step: CustomStringConvertible {
-    let on: Bool
-    let cube: Cube
-
-    var description: String {
-        let x = "x=\(cube.x.lowerBound)..\(cube.x.upperBound - 1)"
-        let y = "y=\(cube.y.lowerBound)..\(cube.y.upperBound - 1)"
-        let z = "z=\(cube.z.lowerBound)..\(cube.z.upperBound - 1)"
-        return "\(on ? "on" : "off") \(x),\(y),\(z)"
+extension ClosedRange {
+    func intersection(_ other: ClosedRange) -> ClosedRange? {
+        if self.contains(other.lowerBound) {
+            return other.lowerBound...Swift.min(self.upperBound, other.upperBound)
+        }
+        if other.contains(self.lowerBound) {
+            return self.lowerBound...Swift.min(self.upperBound, other.upperBound)
+        }
+        return nil
     }
 }
 
 struct Cube {
-    let x: Range<Int>
-    let y: Range<Int>
-    let z: Range<Int>
+
+    let on: Bool
+
+    let x: ClosedRange<Int>
+    let y: ClosedRange<Int>
+    let z: ClosedRange<Int>
 
     var isValid: Bool {
-        x.lowerBound >= -50 && x.upperBound <= 51 &&
-        y.lowerBound >= -50 && y.upperBound <= 51 &&
-        z.lowerBound >= -50 && z.upperBound <= 51
+        x.lowerBound >= -50 && x.upperBound <= 50 &&
+            y.lowerBound >= -50 && y.upperBound <= 50 &&
+            z.lowerBound >= -50 && z.upperBound <= 50
+    }
+
+    var volume: Int {
+        x.count * y.count * z.count
+    }
+
+    func intersection(_ other: Cube) -> Cube? {
+        guard let xIntersection = x.intersection(other.x) else { return nil }
+        guard let yIntersection = y.intersection(other.y) else { return nil }
+        guard let zIntersection = z.intersection(other.z) else { return nil }
+        return Cube(on: other.on, x: xIntersection, y: yIntersection, z: zIntersection)
     }
 
     var coordinates: [Coordinate] {
@@ -43,11 +57,11 @@ struct Coordinate: Hashable {
 }
 
 func solve1(input: String) -> Int {
-    let steps = Parser.parse(input: input)
+    let cubes = Parser.parse(input: input)
     var turnedOn: Set<Coordinate> = []
-    for step in steps where step.cube.isValid {
-        let coordinates = step.cube.coordinates
-        if step.on {
+    for cube in cubes where cube.isValid {
+        let coordinates = cube.coordinates
+        if cube.on {
             turnedOn.formUnion(coordinates)
         } else {
             turnedOn.subtract(coordinates)
@@ -56,6 +70,27 @@ func solve1(input: String) -> Int {
     return turnedOn.count
 }
 
+func calculateVolume(cube: Cube, cubes: [Cube]) -> Int {
+    if cubes.isEmpty {
+        return cube.on ? cube.volume : 0
+    }
+    if cube.on {
+        return cube.volume - calculateVolume(cubes: cubes.compactMap { cube.intersection($0) })
+    } else {
+        return -calculateVolume(cubes: cubes.compactMap { cube.intersection($0) })
+    }
+}
+
+func calculateVolume(cubes: [Cube]) -> Int {
+    var acc = 0
+    for (i, cube) in cubes.enumerated() {
+        print(acc)
+        acc += calculateVolume(cube: cube, cubes: Array(cubes[...(i - 1)]))
+    }
+    return acc
+}
+
 func solve2(input: String) -> Int {
-    return 0
+    let cubes = Parser.parse(input: input)
+    return calculateVolume(cubes: cubes)
 }
--- Y2021/Tests/Day22Tests/SolutionTests.swift
+++ Y2021/Tests/Day22Tests/SolutionTests.swift
@@ -21,15 +21,21 @@ class SolutionTests: XCTestCase {
         XCTAssertEqual(solve1(input: input), 580012)
     }
 
-//    func test_solve2_withSampleInput() {
-//        let path = Bundle.module.path(forResource: "sample", ofType: "txt")!
-//        let input = try! String(contentsOfFile: path, encoding: .utf8)
-//        XCTAssertEqual(solve2(input: input), 0)
-//    }
-//
-//    func test_solve2_withLargeInput() {
-//        let path = Bundle.module.path(forResource: "large", ofType: "txt")!
-//        let input = try! String(contentsOfFile: path, encoding: .utf8)
-//        XCTAssertEqual(solve2(input: input), 0)
-//    }
+    func test_solve2_withSampleInput() {
+        let path = Bundle.module.path(forResource: "sample", ofType: "txt")!
+        let input = try! String(contentsOfFile: path, encoding: .utf8)
+        XCTAssertEqual(solve2(input: input), 39)
+    }
+
+    func test_solve2_withLargerSampleInput() {
+        let path = Bundle.module.path(forResource: "sample3", ofType: "txt")!
+        let input = try! String(contentsOfFile: path, encoding: .utf8)
+        XCTAssertEqual(solve2(input: input), 2758514936282235)
+    }
+
+    func test_solve2_withLargeInput() {
+        let path = Bundle.module.path(forResource: "large", ofType: "txt")!
+        let input = try! String(contentsOfFile: path, encoding: .utf8)
+        XCTAssertEqual(solve2(input: input), 1334238660555542)
+    }
 }

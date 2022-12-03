99a69c955eaf6895e58ac5e9b1f90e913c30bd9b
Daniel Amarante <daniel.amarante@truecaller.com>
2022-12-03 09:18:36 +0100

    Add solution for day 3

--- Y2022/Sources/Day03/Solution.swift
+++ Y2022/Sources/Day03/Solution.swift
@@ -1,9 +1,42 @@
 import Foundation
 
-func solve1(input: String) -> Int {
-    0
+private func getRepeatedItemsInSameBag(input: String) -> [Character] {
+    let lines = input
+        .split(separator: "\n")
+        .map { Array($0) }
+    return lines.map { line in
+        let firstPart = Set(line[0..<line.count / 2])
+        let secondPart = Set(line[line.count / 2..<line.count])
+        return firstPart.intersection(secondPart).first!
+    }
 }
 
-func solve2(input: String) -> Int {
-    0
+private func getRepeatedItemsInGroup(input: String) -> [Character] {
+    let lines = input
+        .split(separator: "\n")
+        .map { Set($0) }
+    var result: [Character] = []
+    for index in stride(from: 0, to: lines.count, by: 3) {
+        let characters = lines[index + 1..<index + 3].reduce(lines[index]) {
+            $0.intersection($1)
+        }
+        result.append(characters.first!)
+    }
+    return result
+}
+
+private func getValue(from character: Character) -> UInt {
+    let asciiValue = UInt(character.asciiValue!)
+    let shift: UInt = character.isUppercase ? 38 : 96
+    return asciiValue - shift
+}
+
+func solve1(input: String) -> UInt {
+    getRepeatedItemsInSameBag(input: input)
+        .reduce(0) { $0 + getValue(from: $1) }
+}
+
+func solve2(input: String) -> UInt {
+    getRepeatedItemsInGroup(input: input)
+        .reduce(0) { $0 + getValue(from: $1) }
 }

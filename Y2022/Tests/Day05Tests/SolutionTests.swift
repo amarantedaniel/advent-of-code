ae4800e68c50a8443fc2cd2820b4fa5d387bd58a
Daniel Amarante <daniel.amarante@truecaller.com>
2022-12-05 17:30:06 +0100

    Add day 5 solution

--- Y2022/Tests/Day05Tests/SolutionTests.swift
+++ Y2022/Tests/Day05Tests/SolutionTests.swift
@@ -1,10 +1,16 @@
-import XCTest
 @testable import Day05
+import XCTest
 
 class SolutionTests: XCTestCase {
+    func test_solve1_withLargeInput() {
+        let path = Bundle.module.path(forResource: "large", ofType: "txt")!
+        let input = try! String(contentsOfFile: path, encoding: .utf8)
+        XCTAssertEqual(solve1(input: input), "DHBJQJCCW")
+    }
+
     func test_solve2_withLargeInput() {
         let path = Bundle.module.path(forResource: "large", ofType: "txt")!
         let input = try! String(contentsOfFile: path, encoding: .utf8)
-        XCTAssertEqual(solve2(input: input), 0)
+        XCTAssertEqual(solve2(input: input), "WJVRLSJJT")
     }
 }

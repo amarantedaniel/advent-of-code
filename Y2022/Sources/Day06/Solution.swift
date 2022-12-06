58eea6e3fb336ce80419aec4c3c82fd4598112af
Daniel Amarante <daniel.amarante@truecaller.com>
2022-12-06 13:44:14 +0100

    Add solution for day 6

--- Y2022/Sources/Day06/Solution.swift
+++ Y2022/Sources/Day06/Solution.swift
@@ -1,9 +1,21 @@
 import Foundation
 
+private func solve(input: String, windowSize: Int) -> Int {
+    for i in 0..<input.count - windowSize {
+        let start = input.index(input.startIndex, offsetBy: i)
+        let end = input.index(input.startIndex, offsetBy: i + windowSize)
+        let window = input[start..<end]
+        if window.count == Set(window).count {
+            return i + windowSize
+        }
+    }
+    fatalError()
+}
+
 func solve1(input: String) -> Int {
-    0
+    solve(input: input, windowSize: 4)
 }
 
 func solve2(input: String) -> Int {
-    0
+    solve(input: input, windowSize: 14)
 }

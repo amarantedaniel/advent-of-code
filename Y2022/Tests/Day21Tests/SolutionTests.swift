1c8c5a64acf4dd6cbd4725ac243b155de2624f51
Daniel Amarante <daniel.amarante@truecaller.com>
2022-12-21 09:42:36 +0100

    Solve day 21

--- Y2022/Tests/Day21Tests/SolutionTests.swift
+++ Y2022/Tests/Day21Tests/SolutionTests.swift
@@ -5,12 +5,12 @@ class SolutionTests: XCTestCase {
     func test_solve1() {
         let path = Bundle.module.path(forResource: "large", ofType: "txt")!
         let input = try! String(contentsOfFile: path, encoding: .utf8)
-        XCTAssertEqual(solve1(input: input), 0)
+        XCTAssertEqual(solve1(input: input), 62386792426088)
     }
 
     func test_solve2() {
         let path = Bundle.module.path(forResource: "large", ofType: "txt")!
         let input = try! String(contentsOfFile: path, encoding: .utf8)
-        XCTAssertEqual(solve2(input: input), 0)
+        XCTAssertEqual(solve2(input: input), 3876027196185)
     }
 }

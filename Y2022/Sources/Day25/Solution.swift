3571017aab80eb7c144f79ad68a5d88d46289b8c
Daniel Amarante <daniel.amarante@truecaller.com>
2022-12-25 16:35:54 +0100

    Solve day 25

--- Y2022/Sources/Day25/Solution.swift
+++ Y2022/Sources/Day25/Solution.swift
@@ -1,9 +1,52 @@
 import Foundation
 
-func solve1(input: String) -> Int {
-    0
+private func toInt(snafu character: Character) -> Int {
+    switch character {
+    case "-":
+        return -1
+    case "=":
+        return -2
+    default:
+        return character.wholeNumberValue!
+    }
 }
 
-func solve2(input: String) -> Int {
-    0
+private func parse(snafu: Substring) -> Int {
+    var result = 0
+    for (exponent, character) in snafu.reversed().enumerated() {
+        result += pow(5, exponent) * toInt(snafu: character)
+    }
+    return result
+}
+
+private func reverse(number: Int) -> String {
+    var number = number
+    var result = ""
+    while number > 0 {
+        let remainder = number % 5
+        switch remainder {
+        case 3:
+            number += 2
+            result = "=\(result)"
+        case 4:
+            number += 1
+            result = "-\(result)"
+        default:
+            result = "\(remainder)\(result)"
+        }
+        number = number / 5
+    }
+    return result
+}
+
+func solve1(input: String) -> String {
+    let number = input
+        .split(separator: "\n")
+        .map(parse(snafu:))
+        .reduce(0, +)
+    return reverse(number: number)
+}
+
+func pow(_ base: Int, _ exponent: Int) -> Int {
+    Int(pow(Double(base), Double(exponent)))
 }
--- Y2022/Tests/Day25Tests/SolutionTests.swift
+++ Y2022/Tests/Day25Tests/SolutionTests.swift
@@ -5,6 +5,6 @@ class SolutionTests: XCTestCase {
     func test_solve1() {
         let path = Bundle.module.path(forResource: "large", ofType: "txt")!
         let input = try! String(contentsOfFile: path, encoding: .utf8)
-        XCTAssertEqual(solve1(input: input), 0)
+        XCTAssertEqual(solve1(input: input), "2-2=12=1-=-1=000=222")
     }
 }

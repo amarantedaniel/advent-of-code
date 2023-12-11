4139f245bc7d7c791f727d0f4259d3c3c57fa0ab
Family Mothership <teste@mothership.lan>
2023-12-11 10:05:37 -0300

    Finish part 2 slow mode

--- Y2023/day-04/src/lib.rs
+++ Y2023/day-04/src/lib.rs
@@ -1,14 +1,19 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
 #[derive(Debug)]
 struct ScratchCard {
+    index: usize,
     winning_numbers: HashSet<u32>,
     own_numbers: HashSet<u32>,
 }
 
 impl ScratchCard {
+    fn matches(&self) -> usize {
+        return self.winning_numbers.intersection(&self.own_numbers).count();
+    }
+
     fn calculate_points(&self) -> usize {
-        let numbers = self.winning_numbers.intersection(&self.own_numbers).count();
+        let numbers = self.matches();
         if numbers <= 2 {
             return numbers;
         }
@@ -24,16 +29,37 @@ pub fn solve_part1(input: &str) -> String {
 }
 
 pub fn solve_part2(input: &str) -> String {
-    return input.to_string();
+    let all_scratch_cards = parse(input);
+    let mut count = 0;
+    let mut scratch_cards = VecDeque::new();
+
+    for i in 0..all_scratch_cards.len() {
+        scratch_cards.push_back(&all_scratch_cards[i]);
+    }
+    while !scratch_cards.is_empty() {
+        count += 1;
+        let scratch_card = scratch_cards.pop_front().unwrap();
+        let matches = scratch_card.matches();
+        for i in scratch_card.index..(scratch_card.index + matches) {
+            scratch_cards.push_back(&all_scratch_cards[i + 1]);
+        }
+    }
+    return count.to_string();
 }
 
 fn parse(input: &str) -> Vec<ScratchCard> {
-    return input.lines().map(parse_line).collect();
+    return input
+        .lines()
+        .enumerate()
+        .map(|(index, line)| parse_line(index, line))
+        .collect();
 }
 
-fn parse_line(line: &str) -> ScratchCard {
-    let line = line.split(":").last().unwrap();
+fn parse_line(index: usize, line: &str) -> ScratchCard {
     let parts = line
+        .split(":")
+        .last()
+        .unwrap()
         .split(":")
         .last()
         .unwrap()
@@ -41,6 +67,7 @@ fn parse_line(line: &str) -> ScratchCard {
         .map(parse_numbers)
         .collect::<Vec<_>>();
     return ScratchCard {
+        index,
         winning_numbers: HashSet::from_iter(parts[0].iter().copied()),
         own_numbers: HashSet::from_iter(parts[1].iter().copied()),
     };

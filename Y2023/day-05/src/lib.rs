ec433786a80fb2758848c919528d7a03c2c195ff
Daniel Amarante <daniel.amarante2@gmail.com>
2023-12-12 12:00:27 -0300

    Part 1 of day 5

--- Y2023/day-05/src/lib.rs
+++ Y2023/day-05/src/lib.rs
@@ -1,6 +1,8 @@
+use std::cmp;
+
 #[derive(Debug)]
 struct Almanac {
-    seeds: Vec<u32>,
+    seeds: Vec<u64>,
     maps: Vec<Map>,
 }
 
@@ -11,15 +13,42 @@ struct Map {
 
 #[derive(Debug)]
 struct MapRow {
-    source_range_start: u32,
-    destination_range_start: u32,
-    range_length: u32,
+    source_range_start: u64,
+    destination_range_start: u64,
+    range_length: u64,
 }
 
 pub fn solve_part1(input: &str) -> String {
     let almanac = parse(input);
-    println!("{:?}", almanac);
-    return input.to_string();
+    let mut lowest = u64::MAX;
+    for seed in almanac.seeds {
+        let location = get_location(seed, &almanac.maps);
+        lowest = cmp::min(lowest, location);
+    }
+    return lowest.to_string();
+}
+
+fn get_location(seed: u64, maps: &Vec<Map>) -> u64 {
+    return maps
+        .iter()
+        .fold(seed, |current, map| apply_map(map, current));
+}
+
+fn apply_map(map: &Map, seed: u64) -> u64 {
+    for row in &map.rows {
+        if let Some(u64) = apply_row(row, seed) {
+            return u64;
+        }
+    }
+    return seed;
+}
+
+fn apply_row(row: &MapRow, seed: u64) -> Option<u64> {
+    let range = row.source_range_start..(row.source_range_start + row.range_length);
+    if range.contains(&seed) {
+        return Some(row.destination_range_start + (seed - row.source_range_start));
+    }
+    return None;
 }
 
 pub fn solve_part2(input: &str) -> String {
@@ -34,15 +63,15 @@ fn parse(input: &str) -> Almanac {
     };
 }
 
-fn parse_seeds(input: &str) -> Vec<u32> {
+fn parse_seeds(input: &str) -> Vec<u64> {
     return input
         .split(" ")
-        .filter_map(|substring| substring.parse::<u32>().ok())
+        .filter_map(|substring| substring.parse::<u64>().ok())
         .collect();
 }
 
 fn parse_maps(input: &[&str]) -> Vec<Map> {
-    return input.iter().map(|line| parse_map).collect();
+    return input.iter().map(|line| parse_map(line)).collect();
 }
 
 fn parse_map(input: &str) -> Map {
@@ -57,7 +86,7 @@ fn parse_map(input: &str) -> Map {
 fn parse_map_row(input: &str) -> MapRow {
     let parts = input
         .split(" ")
-        .filter_map(|number| number.parse::<u32>().ok())
+        .filter_map(|number| number.parse::<u64>().ok())
         .collect::<Vec<_>>();
     return MapRow {
         source_range_start: parts[1],

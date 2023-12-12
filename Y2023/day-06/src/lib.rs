0c6ec43ab0378f85f326ca8d23aaaf80a083536f
Daniel Amarante <daniel.amarante2@gmail.com>
2023-12-12 17:46:32 -0300

    Add parsing

--- Y2023/day-06/src/lib.rs
+++ Y2023/day-06/src/lib.rs
@@ -1,7 +1,33 @@
+use std::iter::zip;
+
+#[derive(Debug)]
+struct Race {
+    time: u32,
+    distance: u32,
+}
+
 pub fn solve_part1(input: &str) -> String {
+    let races = parse(input);
+    println!("{:?}", races);
     return input.to_string();
 }
 
 pub fn solve_part2(input: &str) -> String {
     return input.to_string();
+}
+
+fn parse(input: &str) -> Vec<Race> {
+    let parts = input.lines().collect::<Vec<_>>();
+    let times = parse_line(parts[0]);
+    let distances = parse_line(parts[1]);
+    return zip(times, distances)
+        .map(|(time, distance)| Race { time, distance })
+        .collect();
+}
+
+fn parse_line(line: &str) -> Vec<u32> {
+    return line
+        .split(" ")
+        .filter_map(|number| number.parse::<u32>().ok())
+        .collect();
 }

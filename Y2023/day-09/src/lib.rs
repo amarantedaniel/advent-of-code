9e99809483cc6d24b615001bca7ae60f08b53e3a
Daniel Amarante <daniel.amarante2@gmail.com>
2023-12-16 22:23:06 -0300

    Finish part 2

--- Y2023/day-09/src/lib.rs
+++ Y2023/day-09/src/lib.rs
@@ -6,6 +6,15 @@ pub fn solve_part1(input: &str) -> String {
     return result.to_string();
 }
 
+pub fn solve_part2(input: &str) -> String {
+    let mut result = 0;
+    for numbers in &parse(input) {
+        let reversed = numbers.into_iter().rev().cloned().collect::<Vec<_>>();
+        result += solve(&reversed).last().unwrap();
+    }
+    return result.to_string();
+}
+
 fn solve(numbers: &Vec<i64>) -> Vec<i64> {
     let mut result = numbers.clone();
     let next = generate_next(numbers);
@@ -19,18 +28,19 @@ fn solve(numbers: &Vec<i64>) -> Vec<i64> {
 }
 
 fn generate_next(numbers: &Vec<i64>) -> Vec<i64> {
+    let mut all_are_zero = true;
     let mut result: Vec<i64> = Vec::new();
     for i in 0..(numbers.len() - 1) {
-        result.push(numbers[i + 1] - numbers[i]);
+        let number = numbers[i + 1] - numbers[i];
+        if number != 0 {
+            all_are_zero = false
+        }
+        result.push(number);
     }
-    if numbers.iter().all(|number| number == &0) {
+    if all_are_zero {
         result.push(0);
     }
     return result;
-}
-
-pub fn solve_part2(input: &str) -> String {
-    return input.to_string();
 }
 
 fn parse(input: &str) -> Vec<Vec<i64>> {

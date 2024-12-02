6e2a5411d3100e3e12aec4f10ed8b4a8ce654f9c
Daniel Amarante <daniel.amarante2@gmail.com>
2024-12-02 08:55:34 +0100

    Add day 2

--- Y2024/Sources/Day02/Day02.swift
+++ Y2024/Sources/Day02/Day02.swift
@@ -1,11 +1,46 @@
 import AdventOfCode
 
 struct Day02: AdventDay {
-    func part1(input: String) throws -> String {
-        throw AdventError.notImplemented
+    private func parse(input: String) -> [[Int]] {
+        input.split(separator: "\n").map { line in
+            line.split(separator: " ").compactMap { Int($0) }
+        }
     }
 
-    func part2(input: String) throws -> String {
-        throw AdventError.notImplemented
+    private func isSafe(report: [Int]) -> Bool {
+        let increasing = report[1] > report[0]
+        for i in 0..<report.count - 1 {
+            if increasing {
+                if !(1...3 ~= report[i + 1] - report[i]) {
+                    return false
+                }
+            } else {
+                if !(1...3 ~= report[i] - report[i + 1]) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private func isSafeWhenRemovingLevel(report: [Int]) -> Bool {
+        for i in 0..<report.count {
+            var report = report
+            report.remove(at: i)
+            if isSafe(report: report) {
+                return true
+            }
+        }
+        return false
+    }
+
+    func part1(input: String) throws -> Int {
+        let reports = parse(input: input)
+        return reports.filter(isSafe(report:)).count
+    }
+
+    func part2(input: String) throws -> Int {
+        let reports = parse(input: input)
+        return reports.filter(isSafeWhenRemovingLevel(report:)).count
     }
 }

cf343fb7704615be5c50df6f95db790eba5ade23
Daniel Amarante <daniel.amarante2@gmail.com>
2024-12-03 09:52:51 +0100

    Day 03

--- Y2024/Sources/Day03/Day03.swift
+++ Y2024/Sources/Day03/Day03.swift
@@ -1,11 +1,28 @@
 import AdventOfCode
 
 struct Day03: AdventDay {
-    func part1(input: String) throws -> String {
-        throw AdventError.notImplemented
+    func part1(input: String) throws -> Int {
+        let regex = #/mul\((\d+),(\d+)\)/#
+        return input.matches(of: regex).reduce(0) { result, match in
+            result + Int(match.output.1)! * Int(match.output.2)!
+        }
     }
 
-    func part2(input: String) throws -> String {
-        throw AdventError.notImplemented
+    func part2(input: String) throws -> Int {
+        let regex = #/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/#
+        var isEnabled = true
+        var result = 0
+        for match in input.matches(of: regex) {
+            switch match.output.0 {
+            case "do()":
+                isEnabled = true
+            case "don't()":
+                isEnabled = false
+            default:
+                guard isEnabled else { continue }
+                result += Int(match.output.1!)! * Int(match.output.2!)!
+            }
+        }
+        return result
     }
 }

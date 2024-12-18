8805f491d052f1c6a807d1a8f449e74d37451b2c
Daniel Amarante <daniel.amarante2@gmail.com>
2024-12-18 16:22:31 +0100

    Solve day 18

--- Y2024/Sources/Day16/Day16.swift
+++ Y2024/Sources/Day16/Day16.swift
@@ -1,10 +1,43 @@
 import AdventOfCode
+import Collections
 
-struct Point: Equatable { let x: Int; let y: Int }
+struct Point: Equatable, Hashable, AdditiveArithmetic {
+    static let zero = Point(x: 0, y: 0)
+    let x: Int
+    let y: Int
+
+    static func - (lhs: Point, rhs: Point) -> Point {
+        Point(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+
+    static func + (lhs: Point, rhs: Point) -> Point {
+        Point(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+}
 
 enum Square: Character {
     case wall = "#"
     case path = "."
+}
+
+enum Direction: CaseIterable {
+    case up
+    case down
+    case left
+    case right
+
+    func vector() -> Point {
+        switch self {
+        case .up:
+            return Point(x: 0, y: -1)
+        case .down:
+            return Point(x: 0, y: 1)
+        case .left:
+            return Point(x: -1, y: 0)
+        case .right:
+            return Point(x: 1, y: 0)
+        }
+    }
 }
 
 enum Parser {
@@ -30,18 +63,63 @@ enum Parser {
         }
         return (start, end, map)
     }
+}
 
+struct State: Hashable, Comparable {
+    let distance: Int
+    let direction: Direction
+    let point: Point
+
+    static func < (lhs: State, rhs: State) -> Bool {
+        lhs.distance < rhs.distance
+    }
+
+    static func == (lhs: State, rhs: State) -> Bool {
+        return lhs.point == rhs.point && lhs.direction == rhs.direction
+    }
 }
 
 struct Day16: AdventDay {
-    func part1(input: String) throws -> String {
-        let (start, end, map) = Parser.parse(input: input)
-        print(start)
-        print(end)
-        throw AdventError.notImplemented
+    func dijkstra(map: [[Square]], source: Point, destination: Point) -> Int {
+        var distances: [Point: Int] = [source: 0]
+        var previous: [Point: Point] = [:]
+        var heap = Heap<State>()
+        heap.insert(.init(distance: 0, direction: .right, point: source))
+        while !heap.isEmpty {
+            let current = heap.removeMin()
+            if current.point == destination {
+                return distances[current.point]!
+            }
+            for (direction, neighboor) in neighboors(for: current.point, in: map) {
+                let distance = distances[current.point]! + 1 + (direction == current.direction ? 0 : 1_000)
+                if distance < distances[neighboor, default: Int.max] {
+                    distances[neighboor] = distance
+                    previous[neighboor] = current.point
+                    heap.insert(.init(distance: distance, direction: direction, point: neighboor))
+                }
+            }
+        }
+        fatalError()
     }
 
-    func part2(input: String) throws -> String {
+    private func neighboors(for point: Point, in map: [[Square]]) -> [(Direction, Point)] {
+        var result: [(Direction, Point)] = []
+        for direction in Direction.allCases {
+            let vector = direction.vector()
+            let neighboor = point + vector
+            if map[neighboor.y][neighboor.x] == .path {
+                result.append((direction, neighboor))
+            }
+        }
+        return result
+    }
+
+    func part1(input: String) throws -> Int {
+        let (start, end, map) = Parser.parse(input: input)
+        return dijkstra(map: map, source: start, destination: end)
+    }
+
+    func part2(input: String) throws -> Int {
         throw AdventError.notImplemented
     }
 }
--- Y2024/Sources/Day18/Day18.swift
+++ Y2024/Sources/Day18/Day18.swift
@@ -1,11 +1,92 @@
 import AdventOfCode
+import Collections
+
+enum Constants {
+    static let gridSize = 70
+    static let bytes = 1_024
+}
+
+struct Point: Equatable, Hashable, AdditiveArithmetic {
+    static let zero = Point(x: 0, y: 0)
+    let x: Int
+    let y: Int
+
+    static func - (lhs: Point, rhs: Point) -> Point {
+        Point(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
+    }
+
+    static func + (lhs: Point, rhs: Point) -> Point {
+        Point(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+}
+
+struct State: Hashable, Comparable {
+    let distance: Int
+    let point: Point
+
+    static func < (lhs: State, rhs: State) -> Bool {
+        lhs.distance < rhs.distance
+    }
+
+    static func == (lhs: State, rhs: State) -> Bool {
+        return lhs.point == rhs.point
+    }
+}
 
 struct Day18: AdventDay {
+    private func parse(input: String) -> [Point] {
+        input.split(separator: "\n").map { line in
+            let parts = line.split(separator: ",")
+            return Point(x: Int(parts[0])!, y: Int(parts[1])!)
+        }
+    }
+
+    func countSteps(obstacles: [Point]) -> Int? {
+        let destination = Point(x: Constants.gridSize, y: Constants.gridSize)
+        var distances: [Point: Int] = [.zero: 0]
+        var heap = Heap<State>()
+        heap.insert(.init(distance: 0, point: .zero))
+        while !heap.isEmpty {
+            let current = heap.removeMin()
+            if current.point == destination {
+                return distances[current.point]!
+            }
+            for neighboor in neighboors(of: current.point, obstacles: obstacles) {
+                let distance = distances[current.point]! + 1
+                if distance < distances[neighboor, default: Int.max] {
+                    distances[neighboor] = distance
+                    heap.insert(.init(distance: distance, point: neighboor))
+                }
+            }
+        }
+
+        return distances[destination]
+    }
+
+    private func neighboors(of point: Point, obstacles: [Point]) -> [Point] {
+        [(-1, 0), (1, 0), (0, 1), (0, -1)]
+            .map { y, x in point + Point(x: x, y: y) }
+            .filter { !obstacles.contains($0) }
+            .filter { $0.x >= 0 && $0.y >= 0 && $0.x <= Constants.gridSize && $0.y <= Constants.gridSize }
+    }
+
     func part1(input: String) throws -> String {
-        throw AdventError.notImplemented
+        let obstacles = parse(input: input)
+        return "\(countSteps(obstacles: Array(obstacles[0..<Constants.bytes]))!)"
     }
 
     func part2(input: String) throws -> String {
-        throw AdventError.notImplemented
+        let obstacles = parse(input: input)
+        var lower = Constants.bytes
+        var upper = obstacles.count
+        while upper - lower > 1 {
+            let i = lower + (upper - lower) / 2
+            if countSteps(obstacles: Array(obstacles[0...i])) != nil {
+                lower = i
+            } else {
+                upper = i
+            }
+        }
+        return "\(obstacles[upper].x),\(obstacles[upper].y)"
     }
 }

8c86dfda0782b9481adef5d21d61550bf18bb236
Daniel Amarante <daniel.amarante2@gmail.com>
2024-12-19 20:54:17 +0100

    Solve day 19

--- Y2024/Sources/Day17/Day17.swift
+++ Y2024/Sources/Day17/Day17.swift
@@ -1,8 +1,126 @@
 import AdventOfCode
+import Foundation
+
+struct Memory {
+    var registers: [Register: Int]
+}
+
+enum Register {
+    case a, b, c
+}
+
+enum Operand {
+    case literal(Int)
+    case register(Register)
+}
+
+enum Instruction: Int {
+    case adv = 0
+    case bxl
+    case bst
+    case jnz
+    case bxc
+    case out
+    case bdv
+    case cdv
+}
+
+struct Operation {
+    let instruction: Instruction
+    let operand: Operand
+}
+
+enum Parser {
+    static func parse(input: String) -> (Memory, [Operation]) {
+        let lines = input.split(separator: "\n")
+        let memory = Memory(
+            registers: [
+                .a: Int(lines[0].split(separator: ":")[1].trimmingCharacters(in: .whitespaces))!,
+                .b: Int(lines[1].split(separator: ":")[1].trimmingCharacters(in: .whitespaces))!,
+                .c: Int(lines[2].split(separator: ":")[1].trimmingCharacters(in: .whitespaces))!
+            ]
+        )
+        var operations: [Operation] = []
+        let numbers = lines[3]
+            .split(separator: ":")[1]
+            .split(separator: ",")
+            .compactMap { Int($0.trimmingCharacters(in: .whitespaces)) }
+        var isInstruction = true
+        var instruction: Instruction!
+        for number in numbers {
+            defer { isInstruction.toggle() }
+            if isInstruction {
+                instruction = Instruction(rawValue: number)!
+            } else {
+                switch number {
+                case 0...3:
+                    operations.append(
+                        Operation(instruction: instruction, operand: .literal(number))
+                    )
+                case 4:
+                    operations.append(
+                        Operation(instruction: instruction, operand: .register(.a))
+                    )
+                case 5:
+                    operations.append(
+                        Operation(instruction: instruction, operand: .register(.b))
+                    )
+                case 6:
+                    operations.append(
+                        Operation(instruction: instruction, operand: .register(.c))
+                    )
+                default:
+                    fatalError()
+                }
+            }
+        }
+        return (memory, operations)
+    }
+}
 
 struct Day17: AdventDay {
+    private func execute(memory: inout Memory, operation: Operation) -> Int? {
+        print(operation)
+        switch (operation.instruction, operation.operand) {
+        case (.adv, .literal(let value)):
+            let numerator = memory.registers[.a]!
+            let denominator = Int(powl(2, Double(value)))
+            memory.registers[.a] = numerator / denominator
+        case (.adv, .register(let register)):
+            let numerator = memory.registers[.a]!
+            let denominator = Int(powl(2, Double(memory.registers[register]!)))
+            memory.registers[.a] = numerator / denominator
+        case (.bxl, _):
+            fatalError()
+        case (.bst, .register(let register)):
+            memory.registers[.b] = memory.registers[register]! % 8
+        case (.bst, .literal(let value)):
+            memory.registers[.b] = value % 8
+        case (.jnz, _):
+            fatalError()
+        case (.bxc, _):
+            fatalError()
+        case (.out, .register(let register)):
+            return memory.registers[register]! % 8
+        case (.out, .literal(let value)):
+            return value % 8
+        case (.bdv, _):
+            fatalError()
+        case (.cdv, _):
+            fatalError()
+        }
+        return nil
+    }
+
     func part1(input: String) throws -> String {
-        throw AdventError.notImplemented
+        var (memory, operations) = Parser.parse(input: input)
+        var output: [Int] = []
+        for operation in operations {
+            if let value = execute(memory: &memory, operation: operation) {
+                output.append(value)
+            }
+        }
+        return output.compactMap(\.description).joined(separator: ",")
     }
 
     func part2(input: String) throws -> String {
--- Y2024/Sources/Day19/Day19.swift
+++ Y2024/Sources/Day19/Day19.swift
@@ -1,11 +1,67 @@
 import AdventOfCode
 
+extension Substring {
+    subscript(range: PartialRangeFrom<Int>) -> Substring {
+        let startIndex = self.index(self.startIndex, offsetBy: range.lowerBound)
+        return self[startIndex...]
+    }
+}
+
 struct Day19: AdventDay {
-    func part1(input: String) throws -> String {
-        throw AdventError.notImplemented
+    private func parse(input: String) -> ([String], [String]) {
+        let components = input.components(separatedBy: "\n\n")
+        let patterns = components[0]
+            .components(separatedBy: ", ")
+        let designs = components[1]
+            .components(separatedBy: "\n")
+            .filter { !$0.isEmpty }
+        return (patterns, designs)
     }
 
-    func part2(input: String) throws -> String {
-        throw AdventError.notImplemented
+    private func isPossible(design: Substring, with patterns: [String]) -> Bool {
+        if design.isEmpty {
+            return true
+        }
+        for pattern in patterns where design.starts(with: pattern) {
+            if isPossible(design: design[pattern.count...], with: patterns) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private func countPossibilities(
+        of design: Substring,
+        with patterns: [String],
+        cache: inout [Substring: Int]
+    ) -> Int {
+        if let value = cache[design] {
+            return value
+        }
+        if design.isEmpty {
+            return 1
+        }
+        var count = 0
+        for pattern in patterns where design.starts(with: pattern) {
+            count += countPossibilities(of: design[pattern.count...], with: patterns, cache: &cache)
+        }
+        cache[design] = count
+        return count
+    }
+
+    func part1(input: String) throws -> Int {
+        let (patterns, designs) = parse(input: input)
+        return designs
+            .filter { isPossible(design: $0[...], with: patterns) }
+            .count
+    }
+
+    func part2(input: String) throws -> Int {
+        let (patterns, designs) = parse(input: input)
+        var cache: [Substring: Int] = [:]
+        return designs
+            .reduce(0) {
+                $0 + countPossibilities(of: $1[...], with: patterns, cache: &cache)
+            }
     }
 }

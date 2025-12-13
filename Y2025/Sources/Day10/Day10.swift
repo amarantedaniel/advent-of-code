87546e2aaf39fc84e620704c44b46422267755d9
Daniel Amarante <daniel.amarante2@gmail.com>
2025-12-13 16:05:30 +0100

    Day 11

--- Y2025/Sources/Day10/Day10.swift
+++ Y2025/Sources/Day10/Day10.swift
@@ -1,8 +1,9 @@
 import AdventOfCode
+import Foundation
 
 struct Input {
-    let goal: [Bool]
-    let buttons: [[Int]]
+    let goal: UInt
+    let buttons: [UInt]
 }
 
 struct State {
@@ -22,51 +23,65 @@ struct Day10: AdventDay {
         )
     }
 
-    private func parseState(line: Substring) -> [Bool] {
+    private func parseState(line: Substring) -> UInt {
         guard let match = try? /\[([#.]+)\]/.firstMatch(in: line) else {
             fatalError()
         }
-        return Array(match.1).map { $0 == "#" }
+        return Array(match.1).reversed().enumerated().reduce(0) { acc, value in
+            value.element == "#" ? acc + (1 << value.offset) : acc
+        }
     }
 
-    private func parseButtons(line: Substring) -> [[Int]] {
+    private func parseButtons(line: Substring) -> [UInt] {
         line
             .matches(of: /\((\d+(?:,\d+)*)\)/)
-            .map { $0.1.split(separator: ",").map { Int($0)! } }
+            .map {
+                $0.1
+                    .split(separator: ",")
+                    .map { Int($0)! }
+//                    .reversed()
+                    .reduce(0) { acc, value in acc + (1 << value) }
+            }
     }
 
     func part1(input: String) throws -> Int {
         var result = 0
         for input in parse(input: input) {
-            result += solve(input: input)
+            print(String(input.goal, radix: 2))
+            print("--")
+            for button in input.buttons {
+                print(String(button, radix: 2))
+            }
+            print()
+//            result += solve(input: input)
         }
         return result
     }
 
-    func solve(input: Input) -> Int {
-        var queue: [State] = [State(lights: input.goal.map { _ in false }, count: 0)]
-        while !queue.isEmpty {
-            let state = queue.removeFirst()
-            if state.lights == input.goal {
-                return state.count
-            }
-            for buttons in input.buttons {
-                let lights = apply(button: buttons, state: state.lights)
-                if !queue.contains(where: { $0.lights == lights }) {
-                    queue.append(State(lights: lights, count: state.count + 1))
-                }
-            }
-        }
-        return -1
-    }
-
-    private func apply(button: [Int], state: [Bool]) -> [Bool] {
-        var state = state
-        for index in button {
-            state[index].toggle()
-        }
-        return state
-    }
+//    func solve(input: Input) -> Int {
+//        var queue: [State] = [State(lights: input.goal.map { _ in false }, count: 0)]
+//        while !queue.isEmpty {
+//            let state = queue.removeFirst()
+//            if state.lights == input.goal {
+//                return state.count
+//            }
+//            for buttons in input.buttons {
+//                let lights = apply(button: buttons, state: state.lights)
+//                if !queue.contains(where: { $0.lights == lights }) {
+//                    queue.append(State(lights: lights, count: state.count + 1))
+//                }
+//            }
+//        }
+//        return -1
+//    }
+//
+//    private func apply(button: [Int], state: [Bool]) -> [Bool] {
+//        var state = state
+//        for index in button {
+//            state[index].toggle()
+//        }
+//        return state
+//    }
 
     func part2(input _: String) throws -> Int {
         throw AdventError.notImplemented
--- Y2025/Sources/Day11/Day11.swift
+++ Y2025/Sources/Day11/Day11.swift
@@ -1,11 +1,58 @@
 import AdventOfCode
 
+struct Cache: Hashable {
+    let node: String
+    let required: Set<String>
+}
+
 struct Day11: AdventDay {
-    func part1(input: String) throws -> String {
-        throw AdventError.notImplemented
+    private func parse(input: String) -> [String: [String]] {
+        var map: [String: [String]] = [:]
+        for line in input.split(separator: "\n") {
+            let nodes = line.split(separator: " ")
+            map[String(nodes[0].dropLast())] = nodes[1...].map { String($0) }
+        }
+        return map
     }
 
-    func part2(input: String) throws -> String {
-        throw AdventError.notImplemented
+    func part1(input: String) throws -> Int {
+        let map = parse(input: input)
+        var cache: [Cache: Int] = [:]
+        return countPaths(
+            node: "you", map: map, cache: &cache, required: []
+        )
+    }
+
+    func part2(input: String) throws -> Int {
+        let map = parse(input: input)
+        var cache: [Cache: Int] = [:]
+        return countPaths(
+            node: "svr", map: map, cache: &cache, required: ["fft", "dac"]
+        )
+    }
+
+    private func countPaths(
+        node: String,
+        map: [String: [String]],
+        cache: inout [Cache: Int],
+        required: Set<String>
+    ) -> Int {
+        if let value = cache[Cache(node: node, required: required)] {
+            return value
+        }
+        if node == "out" {
+            return required.isEmpty ? 1 : 0
+        }
+        var count = 0
+        for value in map[node]! {
+            count += countPaths(
+                node: value,
+                map: map,
+                cache: &cache,
+                required: required.subtracting([value])
+            )
+        }
+        cache[Cache(node: node, required: required)] = count
+        return count
     }
 }
